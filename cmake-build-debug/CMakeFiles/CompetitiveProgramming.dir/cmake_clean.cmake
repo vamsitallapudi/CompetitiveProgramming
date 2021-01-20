d618784ae8a27c5d4ecea53dbe73ecf32a5bbe0a
Vamsi Tallapudi <vamsi@coderefer.com>
2021-01-20 09:48:50 +0530

    first commit

---
+++ cmake-build-debug/CMakeFiles/CompetitiveProgramming.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CompetitiveProgramming.dir/Learn/HelloWorld.cpp.o"
+  "CompetitiveProgramming"
+  "CompetitiveProgramming.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CompetitiveProgramming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
