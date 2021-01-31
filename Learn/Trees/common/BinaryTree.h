5566cec5e20636cd8a6cded128d66749d2ab8ee6
Vamsi Tallapudi <vamsi@coderefer.com>
2021-01-31 17:24:22 +0530

    Added comment to show binary tree being formed.

--- Learn/Trees/common/BinaryTree.h
+++ Learn/Trees/common/BinaryTree.h
@@ -1,34 +1,52 @@
 #ifndef BinaryTree_H
 #define BinaryTree_H
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct TreeNode {
+struct TreeNode
+{
     int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode() : val(0),left(nullptr), right(nullptr) { }
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right){}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-TreeNode* formBinaryTree() {
-    struct TreeNode* a = new TreeNode(4);
-    struct TreeNode* b = new TreeNode(5);
-    struct TreeNode* c = new TreeNode(2, a, b);
-    struct TreeNode* d = new TreeNode(3);
-    return new TreeNode(1,c,d);
+TreeNode *formBinaryTree()
+{
+
+    /*              1
+                  /   \
+                 2     3
+               /   \
+              4     5
+    *
+    * 
+    * 
+    * 
+    */
+
+    struct TreeNode *a = new TreeNode(4);
+    struct TreeNode *b = new TreeNode(5);
+    struct TreeNode *c = new TreeNode(2, a, b);
+    struct TreeNode *d = new TreeNode(3);
+    return new TreeNode(1, c, d);
 }
 
-void printTree(TreeNode* x) {
-    if (x) {
-        cout<< x->val<<endl;
+void printTree(TreeNode *x)
+{
+    if (x)
+    {
+        cout << x->val << endl;
     }
-    if (x->left) {
+    if (x->left)
+    {
         printTree(x->left);
     }
-    if (x-> right) {
+    if (x->right)
+    {
         printTree(x->right);
     }
 }
