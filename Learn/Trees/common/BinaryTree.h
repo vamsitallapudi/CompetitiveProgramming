7d6289a1746d2c825e40698b8f209fe430aa6713
Vamsi Tallapudi <vamsi@coderefer.com>
2021-02-17 15:08:50 +0530

    modified header to accommodate BST

--- Learn/Trees/common/BinaryTree.h
+++ Learn/Trees/common/BinaryTree.h
@@ -12,6 +12,7 @@ struct TreeNode
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    TreeNode(int x, TreeNode *left) : val(x), left(left), right(nullptr) {}
 };
 
 TreeNode * initBinaryTree()
@@ -30,6 +31,29 @@ TreeNode * initBinaryTree()
     struct TreeNode *c = new TreeNode(2, a, b);
     struct TreeNode *d = new TreeNode(3);
     return new TreeNode(1, c, d);
+}
+
+TreeNode * initBST()
+{
+
+    /* Forming the following Binary Search Tree:
+                    5
+                  /   \
+                 2     6
+               /   \    \
+              1     4    7
+                   /
+                  3  
+    */              
+
+    struct TreeNode *a = new TreeNode(3);
+    struct TreeNode *b = new TreeNode(4, a);
+    struct TreeNode *e = new TreeNode(1);
+    struct TreeNode *c = new TreeNode(2, e, b);
+    struct TreeNode *d = new TreeNode(7);
+    struct TreeNode *f = new TreeNode(6);
+    f->right = d;
+    return new TreeNode(5, c, f);
 }
 
 void printTree(TreeNode *x)
