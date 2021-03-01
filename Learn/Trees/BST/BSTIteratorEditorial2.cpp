e00735c5edce7d3d42247587cbb886c7f3cf2491
Vamsi Tallapudi <vamsi@coderefer.com>
2021-03-01 15:51:01 +0530

    BST Iterator Editorial 2

---
+++ Learn/Trees/BST/BSTIteratorEditorial2.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include "../common/BinaryTree.h"
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ff              first
+#define ss              second
+#define ll             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+void   c_p_c()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+class BSTIterator {
+    vector<TreeNode* > my_stack;
+    
+    void _leftMostInorder(TreeNode* root) {
+        while(root) {
+            my_stack.push_back(root);
+            root = root->left;
+        }
+    }
+public:
+    BSTIterator(TreeNode* root) {
+        _leftMostInorder(root);
+    }
+    
+    int next() {
+        TreeNode* node = my_stack.back();
+        my_stack.pop_back();
+        if(node->right)
+            _leftMostInorder(node->right);
+        
+        return node->val;
+    }
+    
+    bool hasNext() {
+        return !my_stack.empty();
+    }
+};
+
+
+void solve() {
+    // flattening the bst to array    
+    BSTIterator* bst = new BSTIterator(initBST());
+    if (bst->hasNext()) cout<<"True"<<endl; else cout<<"False"<<endl;
+    cout << bst->next()<<endl;
+    if (bst->hasNext()) cout<<"True"<<endl; else cout<<"False"<<endl;
+    cout << bst->next()<<endl;
+    if (bst->hasNext()) cout<<"True"<<endl; else cout<<"False"<<endl;
+    cout << bst->next()<<endl;
+    while(bst->hasNext()) {
+        cout << bst->next()<<endl;
+    }
+    delete bst;
+}
+int main()
+{
+    c_p_c();
+    solve();
+    return 0;
+}
