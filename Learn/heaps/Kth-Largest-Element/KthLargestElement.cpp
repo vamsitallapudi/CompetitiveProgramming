1bbd3332aa26b70744e3b4632fdce5ddb1a2bf8a
Vamsi Tallapudi <vamsi@coderefer.com>
2021-04-05 13:48:52 +0530

    Kth Largest Element using Heap - NlogK solution

---
+++ Learn/heaps/Kth-Largest-Element/KthLargestElement.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ff first
+#define ss second
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define mii map<int, int>
+#define pqb priority_queue<int>
+#define pqs priority_queue<int, vi, greater<int>>
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+#define mk(arr, n, type) type *arr = new type[n];
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+void c_p_c()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+class Solution
+{
+public:
+    int findKthLargest(vector<int> &nums, int k)
+    {
+        pqs heap;
+        for (auto i : nums)
+        {
+            heap.push(i);
+            if (heap.size() > k)
+            {
+                heap.pop();
+            }
+        }
+        return heap.top();
+    }
+};
+
+void solve()
+{
+    Solution *x = new Solution();
+    vector<int> vect{3, 2, 3, 1, 2, 4, 5, 5, 6};
+    cout << x->findKthLargest(vect, 4);
+}
+int main()
+{
+    c_p_c();
+    solve();
+    return 0;
+}
