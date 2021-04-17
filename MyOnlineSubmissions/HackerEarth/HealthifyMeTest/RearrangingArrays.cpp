7c2ee0a768799d445cd0fc930e8aa81313539c37
Vamsi Tallapudi <vamsi@coderefer.com>
2021-04-17 10:57:55 +0530

    Rearranging Arrays - Healthify Me Hackerearth contest Partially accepted Solution

---
+++ MyOnlineSubmissions/HackerEarth/HealthifyMeTest/RearrangingArrays.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ff first
+#define ss second
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define mii map<int, int>
+#define pqb priority_queue<int>
+#define pqs priority_queue<int, vi, greater<int>>
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+#define mk(arr, n, type) type *arr = new type[n];
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+void c_p_c()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+vector<int> solve(int N, vector<int> A, int X)
+{
+
+    vector<int> sol;
+    int count = 0;
+
+    for (auto i : A)
+    {
+        if (i == X)
+        {
+            count++;
+        }
+        else
+        {
+            sol.push_back(i);
+        }
+    }
+
+    for (size_t i = 0; i < count; i++)
+    {
+        sol.insert(sol.begin(), 1);
+    }
+    // Write your code here
+    return sol;
+}
+
+void solve1()
+{
+    int T;
+    cin >> T;
+    for (int t_i = 0; t_i < T; t_i++)
+    {
+        int N;
+        cin >> N;
+        vector<int> A(N);
+        for (int i_A = 0; i_A < N; i_A++)
+        {
+            cin >> A[i_A];
+        }
+        int X;
+        cin >> X;
+
+        vector<int> out_;
+        out_ = solve(N, A, X);
+        cout << out_[0];
+        for (int i_out_ = 1; i_out_ < out_.size(); i_out_++)
+        {
+            cout << " " << out_[i_out_];
+        }
+        cout << "\n";
+    }
+}
+int main()
+{
+    c_p_c();
+    solve1();
+    return 0;
+}
