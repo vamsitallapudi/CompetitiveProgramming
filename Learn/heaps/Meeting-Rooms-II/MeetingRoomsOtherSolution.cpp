355ea132bedfac289ee76aa82aa934d7357a91a2
Vamsi Tallapudi <vamsi@coderefer.com>
2021-04-19 10:49:18 +0530

    Meeting rooms solution tracking the start and end times

---
+++ Learn/heaps/Meeting-Rooms-II/MeetingRoomsOtherSolution.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ff first
+#define ss second
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define mii map<int, int>
+#define pqb priority_queue<int>
+#define pqs priority_queue<int, vi, greater<int>>
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+#define mk(arr, n, type) type *arr = new type[n];
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+void c_p_c()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+// An interval has start time and end time
+struct Interval
+{
+    int start, end;
+    Interval(int start, int end)
+    {
+        this->start = start;
+        this->end = end;
+    }
+};
+
+class Solution
+{
+public:
+    int minMeetingRooms(vector<Interval> &intervals)
+    {
+        // simply we track at each and every start and end times,
+        // how many rooms are required.
+        map<int, int> changes;
+        for (auto i : intervals)
+        {
+            changes[i.start] += 1;
+            changes[i.end] -= 1;
+        }
+        int rooms = 0, maxrooms = 0;
+        for (auto change : changes)
+            maxrooms = max(maxrooms, rooms += change.second);
+        return maxrooms;
+    }
+};
+void solve()
+{
+    vector<Interval> i;
+    // [26,29],[19,26],[19,28],[4,19],[4,25]]
+    Interval *j = new Interval(26, 29);
+    Interval *k = new Interval(19, 26);
+    Interval *l = new Interval(19, 28);
+    Interval *m = new Interval(4, 19);
+    Interval *n = new Interval(4, 25);
+    i.push_back(*j);
+    i.push_back(*k);
+    i.push_back(*l);
+    i.push_back(*m);
+    i.push_back(*n);
+    Solution *x = new Solution();
+    cout << x->minMeetingRooms(i);
+}
+int main()
+{
+    c_p_c();
+    solve();
+    return 0;
+}
