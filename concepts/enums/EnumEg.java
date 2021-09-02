5fbd7892142103f3b15deb87f99f1af31b045f13
Vamsi Tallapudi <vamsi@coderefer.com>
2021-09-02 08:11:57 +0530

    enum inside a class

---
+++ concepts/enums/EnumEg.java
@@ -0,0 +1,11 @@
+public class EnumEg {
+
+    enum Month {
+        JAN, FEB;
+    }
+
+    public static void main(String[] args) {
+        Month j = Month.JAN;
+        System.out.println(j); // prints JAN
+    }
+}
