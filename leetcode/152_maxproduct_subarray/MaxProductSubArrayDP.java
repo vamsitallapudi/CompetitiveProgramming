f544babf44b51fb1b2015ef221c11870cd7bd3b6
Vamsi Tallapudi <vamsi@coderefer.com>
2021-12-04 19:20:15 +0530

    Max Product Sub Array using DP

---
+++ leetcode/152_maxproduct_subarray/MaxProductSubArrayDP.java
@@ -0,0 +1,26 @@
+public class MaxProductSubArrayDP {
+
+    int result = 0;
+
+    public static void main(String[] args) {
+        MaxProductSubArrayDP sol = new MaxProductSubArrayDP();
+        System.out.println(sol.maxProduct(new int[] { -4, -3, -2 }));
+
+    }
+
+    public int maxProduct(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int minTillNow = nums[0], maxTillNow = nums[0];
+        int result = maxTillNow;
+        for (int i = 1; i < nums.length; i++) {
+            int curr = nums[i];
+            int temp = Math.max(curr, Math.max(maxTillNow * curr, minTillNow * curr));
+            minTillNow = Math.min(curr, Math.min(maxTillNow * curr, minTillNow * curr));
+            maxTillNow = temp;
+            result = Math.max(result, maxTillNow);
+        }
+        return result;
+    }
+}
