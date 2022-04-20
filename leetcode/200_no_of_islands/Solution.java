40d1567dfea0fdd1f27663321b69e942e8f6eecd
Vamsi Tallapudi <vamsi@coderefer.com>
2022-04-20 09:38:22 +0530

    Number of islands using DFS

---
+++ leetcode/200_no_of_islands/Solution.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public int numIslands(char[][] grid) {
+        int islands = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    islands++;
+                    dfs(i, j, grid);
+                }
+            }
+        }
+        return islands;
+    }
+
+    private void dfs(int i, int j, char[][] grid) {
+        if (i < 0 || j < 0 || i > grid.length || j > grid[i].length || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(i + 1, j, grid);
+        dfs(i - 1, j, grid);
+        dfs(i, j + 1, grid);
+        dfs(i, j - 1, grid);
+    }
+
+    public static void main(String[] args) {
+        char[][] grid = {
+                { '1', '1', '1', '1', '0' },
+                { '1', '1', '0', '1', '0' },
+                { '1', '1', '0', '0', '0' },
+                { '0', '0', '0', '0', '0' }
+        };
+        int sol = new Solution().numIslands(grid);
+        System.out.println(sol);
+    }
+}
