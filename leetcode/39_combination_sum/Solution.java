8e8e70380184e9a04f83bbe42db2cdfe8a61ddc3
Vamsi Tallapudi <vamsi@coderefer.com>
2022-07-01 12:35:43 +0530

    Combination sum optimised solution

--- leetcode/39_combination_sum/Solution.java
+++ leetcode/39_combination_sum/Solution.java
@@ -2,35 +2,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-    int[] a;
-    List<List<Integer>> sol = new ArrayList<>();;
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        a = candidates;
-        recHelper(0, target, new ArrayList<>());
+        List<List<Integer>> sol = new ArrayList<>();
+        rec(0, candidates, target, sol, new ArrayList<>());
         return sol;
     }
 
-    private void recHelper(int i, int target, List<Integer> curr) {
-        if (i == a.length) {
-            if (target == 0) {
-                sol.add(new ArrayList<>(curr));
-            }
+    private void rec(int i, int[] c, int t, List<List<Integer>> sol, List<Integer> curr) {
+        if (t == 0) {
+            sol.add(new ArrayList<>(curr));
             return;
         }
+        if (i == c.length) {
+            return;
+        } // base case
+
         // pick
-        if (a[i] <= target) {
-            curr.add(a[i]);
-            recHelper(i, target - a[i], curr);
+        if (c[i] <= t) {
+            curr.add(c[i]);
+            rec(i, c, t - c[i], sol, curr);
             curr.remove(curr.size() - 1);
         }
-        // non pick
-        recHelper(i + 1, target, curr);
+
+        // no pick
+        rec(i + 1, c, t, sol, curr);
     }
 
     public static void main(String[] args) {
-        int[] c = { 2, 3, 6, 7 };
-        List<List<Integer>> sol = new Solution().combinationSum(c, 7);
-        sol.forEach(System.out::println);
+        List<List<Integer>> ans = new Solution().combinationSum(new int[] { 2, 3, 6, 7 }, 7);
+        for (List<Integer> list : ans) {
+            for (Integer i : list) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
     }
 }
