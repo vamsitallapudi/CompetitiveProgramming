aa8b1ba2d5b6031edb86f647988c0a2ad5191466
Vamsi Tallapudi <vamsi@coderefer.com>
2022-07-05 11:55:57 +0530

    Modifications Longest common subsequence using Recursion with memoization

--- leetcode/1143_longest_common_subsequence/Solution.java
+++ leetcode/1143_longest_common_subsequence/Solution.java
@@ -1,39 +1,42 @@
 import java.util.Arrays;
 
 public class Solution {
+    String t1, t2;
     int[][] memo;
-    private String text1;
-    private String text2;
 
     public int longestCommonSubsequence(String text1, String text2) {
-        if (text1.isEmpty() || text2.isEmpty())
+        if (text1.isEmpty() || text2.isEmpty()) {
             return 0;
-
-        this.text1 = text1;
-        this.text2 = text2;
-        memo = new int[text1.length() + 1][text2.length() + 1];
-        // filling array with -1
-        for (int i = 0; i < memo.length - 1; i++) {
-            Arrays.fill(memo[i], 0, memo[0].length - 1, -1);
         }
-        return solve(0, 0);
+        t1 = text1;
+        t2 = text2;
+        memo = new int[t1.length()][t2.length()];
+        for (int[] i : memo) {
+            Arrays.fill(i, -1);
+        }
+        return lcs(0, 0);
     }
 
-    private int solve(int i, int j) {
-        if (memo[i][j] != -1) {
-            return memo[i][j];
+    private int lcs(int a, int b) {
+        if (a == t1.length() || b == t2.length()) {
+            return 0;
+        }
+        if (memo[a][b] != -1) {
+            return memo[a][b];
         }
 
-        // option 1 -> skip this char
-        int option1 = solve(i + 1, j);
-        // option 2 -> consider this char
-        int option2 = 0;
-        int firstOccurence = text2.indexOf(text1.charAt(i), j);
-        if (firstOccurence != -1) {
-            option2 = 1 + solve(i + 1, firstOccurence + 1);
+        // option 1 -> no pick
+        int o1 = lcs(a + 1, b);
+
+        // option 2 -> pick
+        int o2 = 0;
+        // first occurance
+        int fo = t2.indexOf(t1.charAt(a), b);
+        if (fo != -1) {
+            o2 = 1 + lcs(a + 1, fo + 1);
         }
-        memo[i][j] = Math.max(option1, option2);
-        return memo[i][j];
+        memo[a][b] = Math.max(o1, o2);
+        return memo[a][b];
     }
 
     public static void main(String[] args) {
